02e98cbe3c4d1c5e3cad4dff5c3f492b67d18def
Megan Tan <meganytan@gmail.com>
2024-09-18 13:29:33 -0500

    Add routing to application

---
+++ src/app/pages/joke-list/joke-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joke-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './joke-list.component.html',
+  styleUrl: './joke-list.component.css'
+})
+export class JokeListComponent {
+
+}
---
+++ src/app/pages/joke-page/joke-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joke-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './joke-page.component.html',
+  styleUrl: './joke-page.component.css'
+})
+export class JokePageComponent {
+
+}
