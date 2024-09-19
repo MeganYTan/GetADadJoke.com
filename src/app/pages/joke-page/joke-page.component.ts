a46f2f7f68497f3e1eb3c6fe7563cc87171f3d78
Megan Tan <meganytan@gmail.com>
2024-09-18 21:20:52 -0500

    Update joke page component

--- src/app/pages/joke-page/joke-page.component.ts
+++ src/app/pages/joke-page/joke-page.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Joke } from '../../models/joke.model';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
+import { JokeHttpService } from '../../shared/services/joke-http/joke-http.service';
+import { JokeService } from '../../shared/services/joke/joke.service';
 
 @Component({
   selector: 'app-joke-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule],
   templateUrl: './joke-page.component.html',
   styleUrl: './joke-page.component.css'
 })
-export class JokePageComponent {
-
+export class JokePageComponent implements OnInit{
+  joke: Joke | undefined;
+  constructor(
+    private jokeHttpService: JokeHttpService,
+    private route: ActivatedRoute,
+    private jokeService: JokeService
+  ) {}
+  ngOnInit(): void {
+    var jokeId = this.route.snapshot.paramMap.get('id');
+    if(jokeId) {
+      this.jokeHttpService.getJokeById(jokeId).subscribe((data) => {
+        this.joke = data;
+      })
+    } else {
+      // get a random joke
+      this.jokeHttpService.getARandomJoke().subscribe((data) => {
+        this.joke = data;
+      });
+    }
+      
+  }
 }
