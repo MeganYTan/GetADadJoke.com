284a69055c7f8722b34036832b72ece668cb01fc
Megan Tan <meganytan@gmail.com>
2024-09-18 21:18:43 -0500

    Add share button

--- src/app/shared/share-button/share-button.component.spec.ts
+++ src/app/shared/share-button/share-button.component.spec.ts
@@ -1,23 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShareButtonComponent } from './share-button.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ShareButtonComponent', () => {
   let component: ShareButtonComponent;
   let fixture: ComponentFixture<ShareButtonComponent>;
-
+  let mockClipboard: jasmine.Spy;
+  const mockActivatedRoute = {
+    snapshot: {
+      paramMap: {
+        get: (param: string) => null
+      }
+    }
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ShareButtonComponent]
+      declarations: [ShareButtonComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ShareButtonComponent);
     component = fixture.componentInstance;
+    mockClipboard = spyOn(navigator.clipboard, 'writeText').and.returnValue(Promise.resolve());
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should share current URL if jokeId is in url', () => {
+    const jokeId = '123';
+    const activatedRoute = TestBed.inject(ActivatedRoute);
+    activatedRoute.snapshot.paramMap.get = (param: string) => jokeId;
+    component.shareJoke();
+    expect(mockClipboard).toHaveBeenCalledWith(`${window.location.href}`);
+  });
+
+  it('should share joke with URL if jokeId is not in url', () => {
+    component.jokeId = '123';
+    component.shareJoke();
+    const expectedUrl = `${window.location.href}/${component.jokeId}`;
+    expect(mockClipboard).toHaveBeenCalledWith(expectedUrl);
+  })
 });
--- src/app/shared/share-button/share-button.component.ts
+++ src/app/shared/share-button/share-button.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-share-button',
-  standalone: true,
-  imports: [],
   templateUrl: './share-button.component.html',
   styleUrl: './share-button.component.css'
 })
 export class ShareButtonComponent {
-
+  @Input() jokeId: string = '';
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+  shareJoke() {
+    const urlJokeId = this.route.snapshot.paramMap.get('jokeId');
+    if (!urlJokeId) {
+      // get permanent url
+      navigator.clipboard.writeText(`${window.location.href}/${this.jokeId}`);
+    } else {
+      navigator.clipboard.writeText(window.location.href);
+    }
+  }
 }
