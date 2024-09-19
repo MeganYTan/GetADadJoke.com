2b1bb32304e19068b3958a8bcf9e3b80d53d69d8
Megan Tan <meganytan@gmail.com>
2024-09-18 21:21:01 -0500

    Update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RandomJokeButtonContentComponent } from './random-joke-button/random-joke-button-content/random-joke-button-content.component';
+import { FavoriteButtonComponent } from './favorite-button/favorite-button.component';
+import { CopyButtonComponent } from './copy-button/copy-button.component';
+import { ShareButtonComponent } from './share-button/share-button.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RandomJokeButtonContentComponent,
+    FavoriteButtonComponent,
+    CopyButtonComponent,
+    ShareButtonComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    RandomJokeButtonContentComponent,
+    FavoriteButtonComponent,
+    CopyButtonComponent,
+    ShareButtonComponent
   ]
 })
 export class SharedModule { }
